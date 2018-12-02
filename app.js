40f2ea779044132836a0feada62d085638074710
Javier Diaz <hello@coderdiaz.me>
2018-12-01 19:28:25 -0600

    Initial project and clean code

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+module.exports = app;
