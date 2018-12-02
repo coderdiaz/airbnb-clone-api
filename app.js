f507d61f3455956599761c3cf1fa754c0b7ecd8a
Javier Diaz <hello@coderdiaz.me>
2018-12-01 19:47:34 -0600

    Error handling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const createError = require('http-errors');
 
 const app = express();
 
@@ -9,5 +10,20 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.status || 500;
+  // Return error
+  return res.status(statusCode).json({
+    code: statusCode,
+    message: err.message,
+  });
+});
 
 module.exports = app;
